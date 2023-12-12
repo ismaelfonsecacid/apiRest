9c5529866cdda5136f9532cea3fc725733839920
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-12 10:50:49 +0100

    test endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,12 @@
-function register(req,res) {
-    console.log("Se ha ejecutado el resgistro");
+function register(req, res) {
+    // Agrega el mensaje al cuerpo de la respuesta
+    const responseMessage = "Hola has llegado";
 
-    res.status(200).send({msg:"Todo ok"})
+    console.log("Se ha ejecutado el registro");
+
+    // Envía la respuesta al cliente con el mensaje agregado
+    res.status(200).send({ msg: "Todo okardo", additionalMsg: responseMessage });
 }
-
-
-
 
 module.exports = {
     register,
