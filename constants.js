84cd2a2cc0225523c7cfd837c16fe52c6d44c855
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-12 13:19:35 +0100

    access and refreshToken JWT

--- constants.js
+++ constants.js
@@ -5,10 +5,12 @@ const DB_HOST = "cluster0.913ryat.mongodb.net";
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
 
+const JWT_SECRET_KEY = "jfkldf8934ji3"
 module.exports = {
   DB_USER,
   DB_HOST,
   DB_PASSWORD,
   API_VERSION,
   IP_SERVER,
+  JWT_SECRET_KEY
 };
