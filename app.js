ee2c59f67106d89e3cb4e2af0509672b1d95d1b9
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-12 10:16:30 +0100

    bodyParser and static folder

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require("body-parser")
 const { API_VERSION } = require("./constants");
 const app = express();
 
@@ -6,5 +7,16 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hola, pa cuando WORK ADRI');
 });
+
+
+
+
+
+//Configurar body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Configure static folder
+app.use(express.static("uploads"));
 
 module.exports = app;
