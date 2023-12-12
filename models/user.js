0245201a0b65d629e6b81a2db9a06b379a5f46d3
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-12 12:20:06 +0100

    User Model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: String,
+    role: String,
+    active: Boolean,
+    avatar: String,
+});
+
+
+module.exports = mongoose.model("User",UserSchema);
