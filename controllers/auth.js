bb997a3303a0efb9249a3a7724c72efac17d10b1
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-12 12:40:51 +0100

    create Users and send it to mongoDB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,32 @@
-const User = require("../models/user")
-
-function register(req, res) {
-    const {firstname,lastname,email,password} = req.body
-
-    if(!email) res.status(400).send({msg:"Email necesario"})
-    if(!password) res.status(400).send({msg:"Password necesaria"})
-    console.log(req.body);
-
-    const user = new User({
-        firstname,
-        lastname,
-        email: email.toLowerCase(),
-        password,
-        role:"user",
-        active:false
-    })
-
-    console.log(user);
-
-    res.status(200).send({msg:"Todo correcto"})
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+ 
+async function register(req, res) {
+  const { firstName, lastName, email, password } = req.body;
+ 
+  if (!email) res.status(400).send({ msg: "El email es obligatorio" });
+  if (!password) res.status(400).send({ msg: "La contraseña es obligatoria" });
+ 
+  const user = new User({
+    firstName,
+    lastName,
+    email: email.toLowerCase(),
+    role: "user",
+    active: false
+  });
+ 
+  const salt = bcrypt.genSaltSync(10);
+  const hashPassword = bcrypt.hashSync(password, salt);
+  user.password = hashPassword;
+ 
+  try {
+    await user.save();
+    res.status(200).send({ msg: "Usuario guardado" });
+  } catch (err) {
+    res.status(400).send({ msg: `Error al crear el usuario` });
+  }
 }
-
+ 
 module.exports = {
-    register,
-}
-
+  register
+};
