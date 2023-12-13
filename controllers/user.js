e4c85ccfd6740e8c8805d4e16c6c5d624e44bf97
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-13 12:00:04 +0100

    createUser

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs")
+const image = require("../utils/image")
 
 async function getMe(req, res) {
   const { user_id } = req.user;
@@ -27,7 +29,24 @@ async function getUsers(req, res) {
 }
 
 async function createUser(req,res) {
-    
+    const {password} = req.body
+    const salt = bcrypt.genSaltSync(10)
+    const hashPassword = bcrypt.hashSync(password,salt);
+    const user = new User({...req.body,active:false,password : hashPassword})
+
+    if(req.files.avatar) {
+        const imageName = image.getFileName(req.files.avatar)
+        user.avatar = imageName;
+    }
+
+    user.save()
+    .then(userStored => {
+        res.status(200).send(userStored);
+    })
+    .catch(error => {
+        res.status(400).send({ msg: "Error en el proceso de usuario" });
+    });
+
 }
 
 module.exports = {
