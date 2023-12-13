7d784e14be93944d07908d9d29201a80291fe9a5
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-13 09:44:56 +0100

    endpoint: getUsers

--- controllers/user.js
+++ controllers/user.js
@@ -1,19 +1,32 @@
-const User = require("../models/user")
+const User = require("../models/user");
 
-async function getMe(req,res) {
-    const {user_id} = req.user
+async function getMe(req, res) {
+  const { user_id } = req.user;
 
-    const response = await User.findById(user_id);
+  const response = await User.findById(user_id);
 
-    if(!response){
-        res.status(400).send({msg:"No se ha encontrado usuario"});
-    } else {
-        res.status(200).send(response);
-    }
+  if (!response) {
+    res.status(400).send({ msg: "No se ha encontrado usuario" });
+  } else {
+    res.status(200).send(response);
+  }
+}
 
+async function getUsers(req, res) {
+  const { active } = req.query;
 
+  let response = null;
+
+  if (active == undefined) {
+    response = await User.find();
+  } else {
+    response = await User.find({ active });
+  }
+
+  res.status(200).send(response);
 }
 
 module.exports = {
-    getMe,
-}
+  getMe,
+  getUsers,
+};
