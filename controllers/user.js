ce8692a3fbba8327b36a8457fc1c55088fa3ec9b
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-13 12:36:44 +0100

    update user

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const User = require("../models/user");
-const bcrypt = require("bcryptjs")
-const image = require("../utils/image")
+const bcrypt = require("bcryptjs");
+const image = require("../utils/image");
 
 async function getMe(req, res) {
   const { user_id } = req.user;
@@ -28,29 +28,57 @@ async function getUsers(req, res) {
   res.status(200).send(response);
 }
 
-async function createUser(req,res) {
-    const {password} = req.body
-    const salt = bcrypt.genSaltSync(10)
-    const hashPassword = bcrypt.hashSync(password,salt);
-    const user = new User({...req.body,active:false,password : hashPassword})
+async function createUser(req, res) {
+  const { password } = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hashPassword = bcrypt.hashSync(password, salt);
+  const user = new User({ ...req.body, active: false, password: hashPassword });
 
-    if(req.files.avatar) {
-        const imageName = image.getFileName(req.files.avatar)
-        user.avatar = imageName;
-    }
+  if (req.files.avatar) {
+    const imageName = image.getFileName(req.files.avatar);
+    user.avatar = imageName;
+  }
 
-    user.save()
-    .then(userStored => {
-        res.status(200).send(userStored);
+  user
+    .save()
+    .then((userStored) => {
+      res.status(200).send(userStored);
     })
-    .catch(error => {
-        res.status(400).send({ msg: "Error en el proceso de usuario" });
+    .catch((error) => {
+      res.status(400).send({ msg: "Error en el proceso de usuario" });
     });
+}
 
+async function updateUser(req, res) {
+  const { id } = req.params;
+  const userData = req.body;
+
+  // Password
+  if (userData.password) {
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(userData.password, salt);
+    userData.password = hashPassword
+  } else {
+    delete userData.password;
+  }
+
+  // Avatar
+  if(req.files.avatar){
+    const imagePath = image.getFileName(req.files.avatar)
+    userData.avatar = imagePath
+  }
+
+  const response = await User.findByIdAndUpdate({ _id: id }, userData);
+  if (!response) {
+    res.status(400).send({ msg: "Error al actualizar el usuario" });
+  } else {
+    res.status(200).send({ msg: "Actualizacion correcta" });
+  }
 }
 
 module.exports = {
   getMe,
   getUsers,
-  createUser
+  createUser,
+  updateUser,
 };
