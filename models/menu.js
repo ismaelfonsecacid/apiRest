8db6477142d9c2625f7520d51537f7c25028d3b5
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-14 09:37:33 +0100

    menuModel

---
+++ models/menu.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const MenuSchema = mongoose.Schema({
+    title: String,
+    path: String,
+    order: Number,
+    active: Boolean,
+
+})
+
+module.exports = mongoose.model("Menu",MenuSchema);
