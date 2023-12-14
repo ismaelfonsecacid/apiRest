c01faa2e5939757758ab0ffb2c38b54b1e21aaf1
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-14 09:31:38 +0100

    endpoint: delete

--- router/user.js
+++ router/user.js
@@ -11,6 +11,7 @@ api.get("/user/me",[md_auth.asureAuth],UserController.getMe)
 api.get("/users",[md_auth.asureAuth],UserController.getUsers)
 api.post("/user",[md_auth.asureAuth, md_upload],UserController.createUser)
 api.patch("/user/:id",[md_auth.asureAuth,md_upload],UserController.updateUser)
+api.delete("/user/:id",[md_auth.asureAuth,md_upload],UserController.deleteUser)
 
 
 
