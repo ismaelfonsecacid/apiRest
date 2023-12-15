c8935eeaf9d124077dc773da09aa51675b421dc9
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-15 11:43:43 +0100

    create and getMenu

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,8 +1,40 @@
 const Menu = require("../models/menu")
 
+async function createMenu(req,res) {
+    const menu = new Menu(req.body)
+
+    menu
+    .save()
+    .then((menuStored) => {
+      res.status(200).send(menuStored);
+    })
+    .catch((error) => {
+      res.status(400).send({ msg: "Error en el proceso de creacion de menu" });
+    });
+
+}
+
+async function getMenu(req,res) {
+    const {active} = req.query;
+    
+  let response = null;
+
+  if (active == undefined) {
+    response = await Menu.find().sort({order:"asc"});
+  } else {
+    response = await Menu.find({ active });
+  }
+
+  if(!response) {
+    res.status(400).send({msg: 'Not Found'});
+  } else {
+    res.status(200).send(response);
+  }
+}
 
 
 
-
-
-module.exports = {}
+module.exports = {
+    createMenu,
+    getMenu
+}
