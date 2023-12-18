b19203b0b7d49f660c40150ad1357247fa7ff46d
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-18 10:03:02 +0100

    endPoint: Course

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.get("/", (req, res) => {
 const authRoutes = require("./router/auth");
 const userRoutes = require("./router/user");
 const menuRoutes = require("./router/menu");
+const courseRoutes = require("./router/course");
 
 //Configurar body parser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,5 +29,6 @@ app.use(cors());
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
 app.use(`/api/${API_VERSION}`, menuRoutes);
+app.use(`/api/${API_VERSION}`, courseRoutes);
 
 module.exports = app;
