74c82067f43ac6397f383d21155077e9e756633f
Ismael Fonseca Cid <ismanoallaps4@gmail.com>
2023-12-20 10:38:53 +0100

    update

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const image = require("../utils/image");
 const { Storage } = require("@google-cloud/storage");
 const dotenv = require('dotenv');
 dotenv.config();
@@ -21,66 +22,115 @@ const multerStorage = multer.memoryStorage();
 const upload = multer({ storage: multerStorage });
 
 
-async function createPost(req, res){
-  console.log("fds");
+async function createPost(req, res) {
+  const post = new Post(req.body);
+
   try {
-      const post = new Post(req.body);
-      post.created_at = new Date();
-      console.log(req.file);
-        if (req.file) {
-          const fileName = await uploadToStorage(req.file);
-          post.miniature = fileName;
-        }
-        const postStored = await post.save();
-        res.status(200).send(postStored);
-      } catch (error) {
-        console.error(error);
-        res.status(400).send({ msg: "Error en el proceso de post" });
-      }
-
-
-}
-
-
-
-
-
-
-
-
-async function uploadToStorage(file) {
-    return new Promise((resolve, reject) => {
-      const fileName = `post/${Date.now()}_${file.originalname}`;
-      const fileUpload = bucket.file(fileName);
-      const stream = fileUpload.createWriteStream({
-        metadata: {
-          contentType: file.mimetype,
-        },
-      });
-  
-      stream.on("error", (error) => {
-        console.error(error);
-        reject(new Error("Error al subir la imagen a Google Cloud Storage"));
-      });
-  
-      stream.on("finish", () => {
-        resolve(fileName);
-      });
-  
-      stream.end(file.buffer);
-    });
-  }
-  
-  async function deleteFromStorage(fileName) {
-    try {
-      await bucket.file(fileName).delete();
-    } catch (error) {
-      console.error(error);
-      throw new Error("Error al eliminar la imagen de Google Cloud Storage");
+    if (req.file) {
+      const fileName = await uploadToStorage(req.file);
+      post.miniature = fileName;
     }
+    post.created_at = new Date();
+    const postStored = await post.save();
+    res.status(201).send(postStored);
+  } catch (error) {
+    res.status(400).send({ msg: "Error al crear el post" });
   }
-
-
-module.exports = {
-createPost
 }
+
+async function getPosts(req, res) {
+  try {
+    const { page = 1, limit = 10 } = req.query;
+
+    const options = {
+      page: parseInt(page),
+      limit: parseInt(limit),
+      sort: { created_at: "desc" },
+    };
+
+    const postsStored = await Post.paginate({}, options);
+    res.status(200).send(postsStored);
+  } catch (error) {
+    res.status(400).send({ msg: "Error al obtener los posts" });
+  }
+}
+
+async function updatePost(req, res) {
+  try {
+    const { id } = req.params;
+    const postData = req.body;
+
+    if (req.files.miniature) {
+      const imagePath = image.getFileName(req.files.miniature);
+      postData.miniature = imagePath;
+    }
+
+    await Post.findByIdAndUpdate({ _id: id }, postData);
+    res.status(200).send({ msg: "Actualización correcta" });
+  } catch (error) {
+    res.status(400).send({ msg: "Error al actualizar el post" });
+  }
+}
+
+async function deletePost(req, res) {
+  try {
+    const { id } = req.params;
+    await Post.findByIdAndDelete(id);
+    res.status(200).send({ msg: "Post eliminado" });
+  } catch (error) {
+    res.status(400).send({ msg: "Error al eliminar el post" });
+  }
+}
+
+async function getPost(req, res) {
+  try {
+    const { path } = req.params;
+    const postStored = await Post.findOne({ path });
+
+    if (!postStored) {
+      res.status(400).send({ msg: "No se ha encontrado ningun post" });
+    } else {
+      res.status(200).send(postStored);
+    }
+  } catch (error) {
+    res.status(500).send({ msg: "Error del servidor" });
+  }
+}
+async function uploadToStorage(file) {
+  return new Promise((resolve, reject) => {
+    const fileName = `blog/${Date.now()}_${file.originalname}`;
+    const fileUpload = bucket.file(fileName);
+    const stream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: file.mimetype,
+      },
+    });
+
+    stream.on("error", (error) => {
+      console.error(error);
+      reject(new Error("Error al subir la imagen a Google Cloud Storage"));
+    });
+
+    stream.on("finish", () => {
+      resolve(fileName);
+    });
+
+    stream.end(file.buffer);
+  });
+}
+
+async function deleteFromStorage(fileName) {
+  try {
+    await bucket.file(fileName).delete();
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error al eliminar la imagen de Google Cloud Storage");
+  }
+}
+module.exports = {
+  createPost,
+  getPosts,
+  updatePost,
+  deletePost,
+  getPost,
+};
--- router/post.js
+++ router/post.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const PostController = require('../controllers/post');
-const md_auth = require("../middlewares/authenticated");
+const express = require("express");
 const api = express.Router();
+const PostController = require("../controllers/post");
+const md_auth = require("../middlewares/authenticated");
+
 const multer = require('multer');
 const multerStorage = multer.memoryStorage();
 const upload = multer({ storage: multerStorage });
 
+const multiparty = require("connect-multiparty");
+const md_upload = multiparty({ uploadDir: "./uploads/blog" });
 
-api.post("/post",[md_auth.asureAuth,upload.single('post')],PostController.createPost)
+api.post("/post", [md_auth.asureAuth, upload.single('blog')], PostController.createPost);
+api.get("/post", PostController.getPosts);
+api.patch(
+  "/post/:id",
+  [md_auth.asureAuth, md_upload],
+  PostController.updatePost
+);
+api.delete("/post/:id", [md_auth.asureAuth], PostController.deletePost);
+api.get("/post/:path", PostController.getPost);
 
+module.exports = api;
 
-
-module.exports = api
